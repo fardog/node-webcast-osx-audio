ce6892cdf5a18e9802abe66ea15e22fe927b2129
fardog <code@fardogllc.com>
2014-09-23 18:44:04 -0700

    pares down chromecast to webcast-only functions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var Chromecast = function(options) {
+var Webcast = function(options) {
 
   var lame = require('lame');
   var audio = require('osx-audio');
@@ -49,63 +49,7 @@ var Chromecast = function(options) {
 
   var server = app.listen(options.port);
 
-
-  var Client = require('castv2-client').Client;
-  var DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
-  var mdns = require('mdns');
-
-  var browser = mdns.createBrowser(mdns.tcp('googlecast'));
-
-  browser.on('serviceUp', function(service) {
-    console.log('found device "%s" at %s:%d', service.name, service.addresses[0], service.port);
-    ondeviceup(service.addresses[0]);
-    browser.stop();
-  });
-
-  browser.start();
-
-  function ondeviceup(host) {
-
-    var client = new Client();
-
-    client.connect(host, function() {
-      console.log('connected, launching app ...');
-
-      client.launch(DefaultMediaReceiver, function(err, player) {
-        var media = {
-
-          // Here you can plug an URL to any mp4, webm, mp3 or jpg file with the proper contentType.
-          contentId: 'http://' + ip + ':' + server.address().port + '/stream.mp3',
-          contentType: 'audio/mpeg3',
-          streamType: 'LIVE', // or LIVE
-
-          // Title and cover displayed while buffering
-          metadata: {
-            type: 0,
-            metadataType: 0,
-            title: options.name 
-          }        
-        };
-
-        player.on('status', function(status) {
-          console.log('status broadcast playerState=%s', status.playerState);
-        });
-
-        console.log('app "%s" launched, loading media %s ...', player.session.displayName, media.contentId);
-
-        player.load(media, { autoplay: true }, function(err, status) {
-          console.log('media loaded playerState=%s', status.playerState);
-        });
-      });
-
-    });
-
-    client.on('error', function(err) {
-      console.log('Error: %s', err.message);
-      client.close();
-    });
-
-  }
+  console.log("streaming at http://" + ip + ":" + options.port + "/stream.mp3");
 }
 
-module.exports = Chromecast;
+module.exports = Webcast;
